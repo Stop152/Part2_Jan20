f37d1b4fe463abe21e25a562aae68c4277b2f946
Sandra <sandra.smirnova@ymail.com>
2020-01-31 21:12:42 +0200

    Order

--- src/main/java/jtm/activity09/Order.java
+++ src/main/java/jtm/activity09/Order.java
@@ -1,5 +1,7 @@
 package jtm.activity09;
 
+import java.util.Objects;
+
 /*- TODO #1
  * Implement Comparable interface with Order class
  * Hint! Use generic type of comparable items in form: Comparable<Order>
@@ -25,9 +27,78 @@ package jtm.activity09;
  * 
  */
 
-public class Order {
-	String customer; // Name of the customer
-	String name; // Name of the requested item
-	int count; // Count of the requested items
+public class Order implements Comparable<Order> {
+	String customer; // Name of the customer 2
+	String name; // Name of the requested item 1
+	int count; // Count of the requested items 3
 
+	public Order(String customer, String name, Integer count) {
+		this.customer = customer;
+		this.name = name;
+		this.count = count;
+	}
+
+	@Override
+	public int compareTo(Order order) {
+		// TODO Auto-generated method stub
+
+		if (!this.name.equals(order.name)) {
+			return this.name.compareTo(order.name) > 0 ? 1 : -1;
+		}
+
+		if (!this.customer.equals(order.customer)) {
+			return this.customer.compareTo(order.customer) > 0 ? 1 : -1;
+		}
+
+		if (this.count != order.count) {
+			if (this.count > order.count) {
+				return 1;
+			} else {
+				return -1;
+
+			}
+		}
+		return 0;
+	}
+
+	@Override
+	public int hashCode() {
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + count;
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return Objects.hash(customer, name, count);
+	}
+
+	@Override
+	public boolean equals(Object order) {
+		if (this == order)
+			return true;
+		if (order == null)
+			return false;
+		if (getClass() != order.getClass())
+			return false;
+		Order other = (Order) order;
+		if (count != other.count)
+			return false;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ItemName: " + name + "OrdererName: " + customer + "Count " + count;
+	}
+	
 }
